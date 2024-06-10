e0ed7941119624312dc307a5dcc23b6aaf6ea1ed
Jojo Akotua Young <jojoyoung3@gmail.com>
2024-06-10 13:09:33 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    const design1 = document.getElementById('design-1');
+    const design2 = document.getElementById('design-2');
+  
+    function switchDesign() {
+      const viewportWidth = window.innerWidth;
+      if (viewportWidth >= 375) { // Desktop view
+        design1.style.display = 'block';
+        design2.style.display = 'none';
+      } else { // Mobile view
+        design1.style.display = 'none';
+        design2.style.display = 'block';
+      }
+    }
+    switchDesign();
+  });
